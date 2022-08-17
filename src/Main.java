7f79064e2b9352a73142b31275e6a5e03e2428dd
Rinat <bugrishka777@gmail.com>
2022-08-17 20:21:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 400;
+        int bonus = amount / 20;
+        System.out.println("За покупку билета(ов) на сумму: " + amount + " руб.");
+        System.out.println("Вам начислены " + bonus + " бонусных миль.");
+    }
+}
